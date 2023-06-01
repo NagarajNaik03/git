77bb6e3a059401f8757ebb52bed4e00df6e283a1
Nagaraj <nagaraj@qburst.com>
2023-06-01 11:44:04 +0530

    Added script.js file

---
+++ script.js
@@ -0,0 +1,16 @@
+async function getResponse() {
+    const response = await fetch(
+        'https://5f74013db63868001615fe76.mockapi.io/api/v1/products',
+        {
+            method: 'GET',
+           headers: {
+            "Content-Type": "application/json",
+          },
+        }
+    );
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+}
